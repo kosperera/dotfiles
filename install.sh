7f2c7b9a52fcb966125af7238077a77ea1c78c1d
kosalanuwan <kosala.nuwan@gmail.com>
2020-11-22 22:17:43 +0530

    fix: file path not found in remote location

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/kosalanuwan/dotfiles/master/macos.sh)";
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/kosalanuwan/dotfiles/main/macos.sh)";
 
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
@@ -18,7 +18,7 @@ unset file;
 
 if [[ ! -e "/usr/local/bin/brew" ]]; then
   {
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
-                 "$(curl -fsSL https://raw.githubusercontent.com/kosalanuwan/dotfiles/master/brew.sh)";
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/main/install.sh)" &&
+                 "$(curl -fsSL https://raw.githubusercontent.com/kosalanuwan/dotfiles/main/brew.sh)";
   } &> /dev/null
 fi

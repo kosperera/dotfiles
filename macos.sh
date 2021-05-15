d38d503702e93c1f3f2f63c1e33f568e8027b172
kosalanuwan <kosala.nuwan@gmail.com>
2021-05-15 16:07:47 +0530

    fix: adjust keyboard blazing speed

--- macos.sh
+++ macos.sh
@@ -75,8 +75,8 @@ defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Set language and text formats
 # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with

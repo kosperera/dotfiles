0b434399854dd7015f6b0d8c9a9f4ec527218352
kosalanuwan <kosala.nuwan@gmail.com>
2021-05-31 12:37:31 +0530

    chore: add github and npm configurations

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-for file in .{zsh_prompt,exports,aliases,functions,zshrc,gitconfig,gitignore}; do
+for file in .{zsh_prompt,exports,aliases,functions,zshrc,gitconfig,gitignore,gh,npmrc}; do
   /bin/bash -ln s $file ~/$file;
 done;
 unset file;

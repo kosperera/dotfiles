01edcf546c5de90159d5d0ad96ef04f2467019ca
kosalanuwan <kosala.nuwan@gmail.com>
2021-06-01 01:08:25 +0530

    chore: add symlink for gh

--- install.sh
+++ install.sh
@@ -15,10 +15,13 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 for file in .{zsh_prompt,exports,aliases,functions,zshrc,gitconfig,gitignore,gh,npmrc}; do
-  /bin/bash -ln s $file ~/$file;
+  # Symlink instead of replacing.
+  /bin/bash -ln -s $file ~/$file;
 done;
 unset file;
 
+# GitHub is special
+/bin/bash -ln -s ~/.config/gh/config.yml ~/.gh
 
 # Install brew and some tools
 if [[ ! -e "opt/homebrew/bin/brew" ]]; then /bin/bash -c brew.sh; &> /dev/null fi

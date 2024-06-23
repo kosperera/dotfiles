6251a6443e536bc807c6b54d44b83ad8ad9ada96
kosperera <therealkosala@gmail.com>
2024-06-23 03:27:16 -0400

    refactor: simplify create symlink commands

--- setup-symlinks.zsh
+++ setup-symlinks.zsh
@@ -10,19 +10,22 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 DOTFILES_DIR="$HOME/.dotfiles"
 
-# GitHub is special.
+# # Taskfile is special.
+# ln -s -f $DOTFILES_DIR/Taskfile.yml $HOME/Taskfile.yml
+
+# GitHub is also special.
 mkdir -p $HOME/config/gh
-rm -rf $HOME/.config/gh/config.yml && ln -s $DOTFILES_DIR/.gh.c.yml $HOME/.config/gh/config.yml
+ln -s -f $DOTFILES_DIR/.gh.c.yml $HOME/.config/gh/config.yml
 
 # Oh-my-zsh custom.
 for omzfile in {prompt,aliases,exports,functions}.zsh; do
-  rm -rf $ZSH_CUSTOM/$omzfile && ln -s $DOTFILES_DIR/.oh-my-zsh/$omzfile $ZSH_CUSTOM/$omzfile
+  ln -s -f $DOTFILES_DIR/.oh-my-zsh/$omzfile $ZSH_CUSTOM/$omzfile
 done;
 unset omzfile;
 
 # Symlink the rest.
 for dotfile in .{zshrc,gitconfig,gitignore,npmrc,nvmrc}; do
-  rm -rf $HOME/$dotfile && ln -s $DOTFILES_DIR/$dotfile $HOME/$dotfile
+  ln -s -f $DOTFILES_DIR/$dotfile $HOME/$dotfile
 done;
 unset dotfile;
 

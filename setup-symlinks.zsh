cb088e9ecacc5f3ce5f951aeb412cae40910fc4a
kosperera <therealkosala@gmail.com>
2025-02-18 15:23:42 -0500

    style: improve formatting

--- setup-symlinks.zsh
+++ setup-symlinks.zsh
@@ -12,15 +12,12 @@ DOTFILES_DIR="$HOME/.dotfiles"
 
 # Make the terminal smarter.
 ln -s -f $DOTFILES_DIR/.oh-my-zsh/makesmarter.zsh ${ZSH_CUSTOM:-"~/.oh-my-zsh/custom"}/makesmarter.zsh
-
 # GitHub is very special.
 mkdir -p $HOME/.config/gh
 ln -s -f $DOTFILES_DIR/.gh.c.yml $HOME/.config/gh/config.yml
-
 # Symlink the rest.
 ln -s -f $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 ln -s -f $DOTFILES_DIR/.gitignore $HOME/.gitignore
-
 # Symlink the zsh config files.
 # To customize prompt, run `p10k configure` or edit .p10k.zsh.
 ln -s -f $DOTFILES_DIR/.p10k.zsh $HOME/.p10k.zsh
